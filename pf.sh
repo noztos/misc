d32a7abcaca2fffe7a4050db1766a4b808bc420d
noztos <noz.tos@gmail.com>
2013-11-14 00:24:25 +0900

    add

---
+++ pf.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# usage : ssh-agent pf.sh
+PFSH=remoteHostUsr
+PFHOST=remoteHost
+PFRPORT=remoteHostPort
+PFSSHKEY=sshIdentKey
+SSH_PATH=/usr/bin
+BIN=/bin
+
+${SSH_PATH}/ssh-add ${SSH_PATH}
+trap "echo trapped\(INT\) at; date" INT
+trap "echo trapped\(TSTP\) at; date" TSTP
+
+sleep 60
+while :
+do
+  #${SSH_PATH}/ssh -R ${PFRPORT}:localhost:22 -N ${PFSH}@${PFHOST}
+  ${SSH_PATH}/ssh -R ${PFRPORT}:localhost:22 -o "ServerAliveInterval 100" -N ${PFSH}@${PFHOST}
+  echo disconnected at; ${BIN}/date
+  ${BIN}/sleep 30
+done
